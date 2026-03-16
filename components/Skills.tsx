e3bd1408550241a9fdd33dd52bdf375c927dcaa4
Yash <yashsolanki0728@gmail.com>
2026-03-16 15:57:21 +0530

    feat: update Resume and Skills components with improved styling and structure

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -2,15 +2,44 @@
 
 import { motion } from "framer-motion";
 
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaReact,
+  FaNodeJs,
+  FaGitAlt
+} from "react-icons/fa";
+
+import {
+  SiNextdotjs,
+  SiTypescript,
+  SiTailwindcss,
+  SiMongodb,
+  SiRedux,
+  // SiMicrosoftazuredevops ,
+  SiSocketdotio
+} from "react-icons/si";
+
 const skills = [
-  "HTML", "CSS", "JavaScript", "React.js", "Next.js", "TypeScript", "Tailwind",
-  "Node.js", "MongoDB", "WebSockets", "Redux",
-  "Azure", "Git",
+  { name: "HTML", icon: FaHtml5 },
+  { name: "CSS", icon: FaCss3Alt },
+  { name: "JavaScript", icon: FaJs },
+  { name: "React.js", icon: FaReact },
+  { name: "Next.js", icon: SiNextdotjs },
+  { name: "TypeScript", icon: SiTypescript },
+  { name: "Tailwind", icon: SiTailwindcss },
+  { name: "Node.js", icon: FaNodeJs },
+  { name: "MongoDB", icon: SiMongodb },
+  { name: "WebSockets", icon: SiSocketdotio },
+  { name: "Redux", icon: SiRedux },
+  // { name: "Azure", icon: SiMicrosoftazuredevops},
+  { name: "Git", icon: FaGitAlt },
 ];
 
 export default function Skills() {
   return (
-    <section id="skills" className="py-10 md:py-20 bg-gray-100 dark:bg-gray-800">
+    <section id="skills" className="py-10 md:py-20 bg-white dark:bg-gray-800">
       <div className="max-w-7xl mx-auto px-4 md:px-7">
         <h2 className="text-3xl font-bold mb-10">Skills</h2>
 
@@ -25,19 +54,21 @@ export default function Skills() {
           }}
           className="grid grid-cols-2 md:grid-cols-4 gap-6"
         >
-          {skills.map((skill) => (
-            <motion.div
-              key={skill}
+          {skills.map((skill) => { 
+            const Icon = skill.icon;
+             return (<motion.div
+              key={skill.name}
               variants={{
                 hidden: { opacity: 0, y: 20 },
                 visible: { opacity: 1, y: 0 },
               }}
-              whileHover={{ y: -6, scale: 1.05 }}
-              className="p-3 md:p-4 bg-white dark:bg-gray-900 rounded-xl text-center shadow-sm cursor-pointer text-blue-500 dark:text-white font-medium"
+              whileHover={{ y: -3, scale: 1.05 }}
+             className="p-4 bg-white dark:bg-gray-900 rounded-lg text-center border border-gray-200 dark:border-gray-700 cursor-pointer text-blue-500 dark:text-white font-medium flex items-center justify-center gap-2"
             >
-              {skill}
-            </motion.div>
-          ))}
+              <Icon className="text-2xl" />
+              {skill.name}
+            </motion.div>)
+            })}
         </motion.div>
       </div>
     </section>
